19c02c690bd09bc6057adc2f7c6f984f29879640
Nicolas Soler <jnsoler04@gmail.com>
2024-01-04 05:43:59 -0500

    pre deployment

---
+++ gruntfile.js
@@ -0,0 +1,51 @@
+const webpackDev = require('./webpack.config.js');
+const path = require('path');
+var webpack = require("webpack");
+console.log('Running', process.env.NODE_ENV, 'build');
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            server: {
+                options: {
+                    port: 3030,
+                    base: 'dist/',
+                }
+            }
+        },
+        webpack: {
+            myConfig: webpackDev,
+        },
+        watch: {
+            options: {
+                livereload: true,
+            },
+            js: {
+                files: ['src/**/*.js'],
+                tasks: ['webpack'],
+                options: {
+                    interrupt: true,
+                },
+            },
+
+        },
+        'gh-pages': {
+            options: {
+                base: 'dist',
+                add: true
+            },
+            src: ['**/*']
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-gh-pages');
+
+    grunt.loadNpmTasks('grunt-webpack');
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    grunt.registerTask('default', ['webpack', 'connect', 'watch']);
+};
