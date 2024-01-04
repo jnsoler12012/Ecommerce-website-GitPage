c6163ab9c0161ccf5fd72d32aaa936d90a29e720
Nicolas Soler <jnsoler04@gmail.com>
2024-01-04 06:39:47 -0500

    Updates

--- src/UI/Components/index.js
+++
@@ -1,7 +0,0 @@
-import { default as InteractiveRouterLink } from './InteractiveRouterLink'
-import { default as Navbar } from './Navbar'
-
-
-export {
-    InteractiveRouterLink, Navbar
-}
--- src/UI/Pages/index.js
+++
@@ -1,7 +0,0 @@
-import { default as Home } from './Home'
-import { default as ProductDetails } from './ProductDetails'
-import { default as ErrorPage } from './ErrorPage'
-
-export {
-    Home, ProductDetails, ErrorPage
-}
