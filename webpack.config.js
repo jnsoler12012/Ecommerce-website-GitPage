742a19f579f01a72b9e195b0554e4f9331f4cd86
Nicolas Soler <jnsoler04@gmail.com>
2024-01-05 13:01:55 -0500

    pre deployment - image fix #7

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,7 @@ module.exports = {
                         loader: 'file-loader',
                         options: {
                             name: `[contenthash].[ext]`,
-                            publicPath: '/dist/',
+                            publicPath: 'Ecommerce-website-GitPage/dist',
                         },
                     }
                 ]
