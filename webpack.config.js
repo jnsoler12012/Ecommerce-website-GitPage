68ea20df9a22008bc06f152b8e52023f95a74b7c
Nicolas Soler <jnsoler04@gmail.com>
2024-01-04 22:38:11 -0500

    pre deployment - image fix #2

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         path: `${__dirname}/dist`,
         clean: true,
         //publicPath: '/dist/',
-        publicPath: '/dist/',
+        publicPath: '/',
         filename: 'bundle.js',
     },
     devServer: {
